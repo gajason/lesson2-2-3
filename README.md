63abdb93c03eb87937f91a2635887aca6a1c8427
Artyom MG <gajason2@gmail.com>
2023-01-02 13:38:37 +0300

    removed comments

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # lesson-2-2-3
+# some comments are changed
 Homework for lesson 2-2-3
+
--- lesson_2-2-3.py
+++ lesson_2-2-3.py
@@ -2,10 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 import time 
-#import math
 
 link = "https://suninjuly.github.io/selects1.html"
-
 
 try:
     browser = webdriver.Chrome()
@@ -17,15 +15,9 @@ try:
     print(int(num1.text) + int(num2.text))
     select.select_by_value(str(int(num1.text) + int(num2.text)))
 
-		# можно и так: 
-		# valuex = browser.find_element(BY.CSS_SELECTOR, '[id = "treasure"]').get_attribute('valuex')
-		# x_element = browser.find_element(By.ID, "treasure")
     browser.find_element(By.XPATH, '//div//form//button').click()
 
-
-
 finally:
-    # успеваем скопировать код за 30 секунд
+	# waiting 30 secs
     time.sleep(30)
-    # закрываем браузер после всех манипуляций
     browser.quit()
